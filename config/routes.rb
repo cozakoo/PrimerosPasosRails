b7d4b68d59d4f6ea8f401beba533750a598f44c2
Martin_arcos <martinarcosvargas2@gmail.com>
2022-10-01 20:57:36 -0300

    preparacion de proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #resources: users --crea todas las rutas se CRUD
+  # Genero solo las rutas de new y create
+  resource :users, only:[:new, :create]
 end
