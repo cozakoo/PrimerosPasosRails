9716b7e9fb2f8c8d80e2f6bff65839d5e7b91184
Martin_arcos <martinarcosvargas2@gmail.com>
2022-10-18 12:36:49 -0300

    Registro de un usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
+    end
+
+    def create
+       @user = User.create(params.require(:user).permit(:name, :email, :password)) 
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_224328) do
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email"
-  end
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_152325) do
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'digest' for column 'password'
 
 end
